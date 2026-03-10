cc95e07e24a4558a4acb4bf757326d088063e8d6
NYKK3 <nykk3.it@gmail.com>
2026-03-10 18:08:19 +0100

    Aggiunti molti altri comandi.

--- Main.cs
+++ Main.cs
@@ -141,36 +141,28 @@ namespace GTA5ComandiVocaliExtension
             if (data == "in_servizio") { SafeCall(() => GrammarPolice.API.Functions.Available(true, true)); }
             if (data == "fuori_servizio") { SafeCall(() => GrammarPolice.API.Functions.Busy(true, true)); }
             if (data == "accetta_chiamata") { SafeCall(() => Callout.Accept()); }
-
-            // Poco per volta cambiare i nomi dei comandi
             if (data == "in_pattuglia") { SafeCall(() => GrammarPolice.API.Functions.StartPatrol(true, true)); }
             if (data == "investigando") { SafeCall(() => GrammarPolice.API.Functions.Investigating(true, true)); }
             if (data == "sulla_scena") { SafeCall(() => GrammarPolice.API.Functions.Scene(true, true)); }
             if (data == "torno_in_centrale") { SafeCall(() => GrammarPolice.API.Functions.ReturnToStation(true, true)); }
             if (data == "in_pericolo") { SafeCall(() => GrammarPolice.API.Functions.Panic()); }
             if (data == "agente_a_terra") { SafeCall(() => Backup.OfficerDown()); }
-
             if (data == "fermato_un_veicolo") { SafeCall(() => GrammarPolice.API.Functions.TrafficStop(true, true)); }
-            if (data == "fermato_un_veicolo_backup") { SafeCall(() => UltimateBackup.API.Functions.callTrafficStopBackup()); }
+            if (data == "fermato_un_veicolo_backup2") { SafeCall(() => UltimateBackup.API.Functions.callTrafficStopBackup()); }
+            if (data == "fermato_un_veicolo_backup") { SafeCall(() => Backup.FelonyStopBackup()); } // potrebbe essere simile a questa sopra
             if (data == "codice2") { SafeCall(() => Backup.Code2()); }
             if (data == "codice3") { SafeCall(() => Backup.Code3()); }
             if (data == "codice4") { SafeCall(() => Status.Code4()); }
-
+            if (data == "swat") { SafeCall(() => Backup.Swat()); }
+            if (data == "noose") { SafeCall(() => Backup.NOOSE()); }
+            if (data == "controllo_radio") { SafeCall(() => Status.RadioCheck()); }
             if (data == "invia_backup") { SafeCall(() => UltimateBackup.API.Functions.callPursuitBackup()); }
             if (data == "invia_elicottero") { SafeCall(() => Backup.Air()); }
             if (data == "invia_medico_legale") { SafeCall(() => Backup.Coroner()); }
             if (data == "invia_ambulanza") { SafeCall(() => Backup.EMS()); }
             if (data == "invia_vigili_del_fuoco") { SafeCall(() => Backup.Fire()); }
-
-            if (data == "controllo_documenti_persona")
-            {
-                SafeCall(() => StopThePed.API.Functions.requestDispatchPedCheck(true));
-            }
-            if (data == "controllo_targa_veicolo")
-            {
-                SafeCall(() => StopThePed.API.Functions.requestDispatchVehiclePlateCheck(true));
-            }
-
+            if (data == "controllo_documenti_persona") { SafeCall(() => StopThePed.API.Functions.requestDispatchPedCheck(true)); }
+            if (data == "controllo_targa_veicolo") { SafeCall(() => StopThePed.API.Functions.requestDispatchVehiclePlateCheck(true)); }
             if (data == "inseguimento") { SafeCall(() => Callout.InitiatePursuit()); }
             if (data == "sospetto_scappato") { SafeCall(() => Advisory.SuspectLost()); }
             if (data == "sospetto_catturato") { SafeCall(() => Advisory.SuspectCustody()); }
@@ -179,19 +171,29 @@ namespace GTA5ComandiVocaliExtension
             if (data == "manovra_pit") { SafeCall(() => Backup.Pit()); }
             if (data == "invia_carroattrezzi") { SafeCall(() => Backup.Tow()); }
             if (data == "invia_trasporto") { SafeCall(() => Backup.Transport()); }
-
             if (data == "nuovo_veicolo") { SafeCall(() => Backup.Vehicle()); }
-
             if (data == "pausa_cibo") { SafeCall(() => Status.MealBreak()); }
-
             if (data == "invia_meccanico") { SafeCall(() => MechanicBackup.API.spawnMechanicUnit()); }
             if (data == "invia_assicurazione") { SafeCall(() => Backup.Insurance()); }
             if (data == "invia_gruppo_supporto") { SafeCall(() => Backup.GroupBackup()); }
-            if (data == "invia_controllo_animali") { SafeCall(() => StopThePed.API.Functions.callAnimalControl()); }
+            if (data == "invia_controllo_animali") { SafeCall(() => Backup.Animal()); }
+            if (data == "si") { SafeCall(() => Interactions.Yes()); }
+            if (data == "no") { SafeCall(() => Interactions.No()); }
+            if (data == "multa") { SafeCall(() => Interactions.TicketNumber()); }
+            if (data == "taser") { SafeCall(() => Interactions.Taser()); }
+            if (data == "contatto_visivo") { SafeCall(() => Advisory.Visual()); }
+            if (data == "persona_ferita") { SafeCall(() => Advisory.PersonInjured()); }
+            if (data == "persona_deceduta") { SafeCall(() => Advisory.PersonDeceased()); }
+            if (data == "lotta") { SafeCall(() => Advisory.Fight()); }
+            if (data == "stop_traffico") { SafeCall(() => Traffic.Stop()); }
+            if (data == "rallenta_traffico") { SafeCall(() => Traffic.Slow()); }
+            if (data == "riprendi_traffico") { SafeCall(() => Traffic.Clear()); }
+            if (data == "controllo_traffico") { SafeCall(() => Traffic.Control()); }
+            if (data == "ufficiale_donna") { SafeCall(() => Backup.Female()); }
+            if (data == "k9") { SafeCall(() => Backup.K9()); }
 
             // Comandi per i ped
             if (data == "dismissAllBackupUnits") { SafeCall(() => UltimateBackup.API.Functions.dismissAllBackupUnits()); }
-
             
             return Encoding.UTF8.GetBytes("OK");
         }
